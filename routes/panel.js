e482deee17aaa4317b6437c7c2cd0989d8467e43
zana nouri <111075978+zana-nouri@users.noreply.github.com>
2023-03-29 17:05:28 +0330

    Update panel.js

--- routes/panel.js
+++ routes/panel.js
@@ -4,6 +4,7 @@ const path = require("path");
 const product = [];
 router.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "../", "views", "panel.html"));
+  product.push(req.body.fistName)
 });
 
 exports.route = router;
